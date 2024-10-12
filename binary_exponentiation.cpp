21c57095d97f758b3883ab7be8a3d119e259b00a
PrithviS18 <prithvisen003@gmail.com>
2024-10-12 13:01:16 +0530

    changes

--- binary_exponentiation.cpp
+++ binary_exponentiation.cpp
@@ -9,13 +9,24 @@ long long power(long long A, long long B)
         return res * res;
 }
 
-ll power(ll A, ll B,ll mod)
+int power(int x, int y, int p)
 {
-    if (B == 0)
-        return 1;
-    long long res = power(A, B / 2);
-    if (B % 2)
-        return (res * res * A)%mod;
-    else
-        return (res * res)%mod;
+ 
+    // Initialize answer
+    int res = 1;
+ 
+    // Check till the number becomes zero
+    while (y > 0) {
+ 
+        // If y is odd, multiply x with result
+        if (y % 2 == 1)
+            res = (res * x);
+ 
+        // y = y/2
+        y = y >> 1;
+ 
+        // Change x to x^2
+        x = (x * x);
+    }
+    return res % p;
 }
