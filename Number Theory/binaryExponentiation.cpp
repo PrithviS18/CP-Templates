2e1c520c88b1b68dd881440a616ec75c4f0c77e9
PrithviS18 <prithvisen003@gmail.com>
2024-10-13 10:36:15 +0530

    changed folder structure

--- Number Theory/binaryExponentiation.cpp
+++ Number Theory/binaryExponentiation.cpp
@@ -11,21 +11,11 @@ long long power(long long A, long long B)
 
 int power(int x, int y, int p)
 {
- 
-    // Initialize answer
     int res = 1;
- 
-    // Check till the number becomes zero
     while (y > 0) {
- 
-        // If y is odd, multiply x with result
         if (y % 2 == 1)
             res = (res * x);
- 
-        // y = y/2
         y = y >> 1;
- 
-        // Change x to x^2
         x = (x * x);
     }
     return res % p;
