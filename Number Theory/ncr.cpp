c91880e7737bbe91882ef1ee125192cc6df962fe
PrithviS18 <prithvisen003@gmail.com>
2024-10-16 00:12:24 +0530

    added ncr

---
+++ Number Theory/ncr.cpp
@@ -0,0 +1,25 @@
+vector<ll> fact(N, 0), invfact(N, 0);
+ll power(ll x, ll y){
+    ll res = 1;
+    x = x % mod;
+    while(y > 0){
+        if(y & 1)
+            res = res * x % mod;
+        x = x * x % mod;
+        y = y >> 1;
+    }
+    return res;
+}
+void factorials(){
+    fact[0] = 1;
+    for(int i=1;i<N;i++) 
+        fact[i] = (i * 1LL * fact[i - 1]) % mod;
+    invfact[N-1] = power(fact[N-1], mod - 2);
+    for(int i=N-2;i>=0;i--) 
+        invfact[i] = ((i + 1) * 1LL * invfact[i + 1]) % mod;
+}
+ll ncr(ll n, ll r) {
+    if(r > n || n < 0 || r < 0)
+        return 0;
+    return ((fact[n] * invfact[r]) % mod * invfact[n - r]) % mod;
+}
