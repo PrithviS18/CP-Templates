1bd86d81c6461c61410189e27e38ab1d6dd1a68e
PrithviS18 <prithvisen003@gmail.com>
2024-10-19 12:33:02 +0530

    changed

--- Number Theory/primeFactorisation.cpp
+++ Number Theory/primeFactorisation.cpp
@@ -1,14 +1,14 @@
 void primeFactors(int n) 
 { 
-    while (n % 2 == 0) { 
-        cout << 2 << " "; 
+    if (n%2==0)vec.push_back(2);
+    while (n % 2 == 0) {  
         n = n/2; 
     }  
     for (int i = 3; i <= sqrt(n); i = i + 2) { 
+        if (n%i==0)vec.push_back(i);
         while (n % i == 0){ 
-            cout << i << " "; 
             n = n/i; 
         } 
     } 
-    if (n > 2)cout << n << " "; 
+    if (n > 2)vec.push_back(i);
 } 
