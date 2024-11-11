f5aa88dc9f925e12d8c6f0dfbcc312dfefa00b32
PrithviS18 <prithvisen003@gmail.com>
2024-11-11 20:46:31 +0530

    xor hashing

---
+++ Others/xorHashing.cpp
@@ -0,0 +1,5 @@
+long long rng() {
+	static std::mt19937 gen(
+	    std::chrono::steady_clock::now().time_since_epoch().count());
+	return std::uniform_int_distribution<long long>(0, INT64_MAX)(gen);
+}
