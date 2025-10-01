057d2154adc26b06272d8403ab06d1059c2bd599
PrithviS18 <prithvisen003@gmail.com>
2025-10-01 12:27:16 +0530

    ncr changes

--- Number Theory/ncr.cpp
+++ Number Theory/ncr.cpp
@@ -1,43 +1,51 @@
-vector<ll> fact(N, 0), invfact(N, 0);
+const int mod = 1e9 + 7;
 
-ll mod_mul(ll a, ll b){
-    a = a % mod;
-    b = b % mod;
-    return (((a * b) % mod) + mod) % mod;
-}
- 
-ll mod_add(ll a, ll b){
-    a = a % mod;
-    b = b % mod;
-    return (((a + b) % mod) + mod) % mod;
+int add(int a, int b) {
+    if (a + b >= mod)
+        return a + b - mod;
+    return a + b;
 }
 
-ll mod =  1e9+7;
-const ll N = 2e5+1;
-ll fact[N],invfact[N];
+int sub(int a, int b) {
+    if (a < b)
+        return a + mod - b;
+    return a - b;
+}
 
-ll power(ll x, ll y){
-    ll res = 1;
-    x = x % mod;
-    while(y > 0){
-        if(y & 1)
-            res = res * x % mod;
-        x = x * x % mod;
-        y = y >> 1;
+int mul(int a, int b) {
+    return (int)((1ll * a * b) % mod);
+}
+
+int binpow(int a, int pw) {
+    int b = 1;
+    while (pw) {
+        if (pw & 1)
+            b = mul(b, a);
+        a = mul(a, a);
+        pw >>= 1;
     }
-    return res;
-}
-void factorials(){
-    fact[0] = 1;
-    for(int i=1;i<N;i++) 
-        fact[i] = (i * 1LL * fact[i - 1]) % mod;
-    invfact[N-1] = power(fact[N-1], mod - 2);
-    for(int i=N-2;i>=0;i--) 
-        invfact[i] = ((i + 1) * 1LL * invfact[i + 1]) % mod;
+    return b;
 }
 
-ll ncr(ll n, ll r) {
-    if(r > n || n < 0 || r < 0)
+int inv(int a) {
+    return binpow(a, mod - 2);
+}
+
+
+const int N = 15000;
+int f[N], r[N];
+
+void precalc() {
+    f[0] = 1;
+    for (int i = 1; i < N; i++)
+        f[i] = mul(f[i - 1], i);
+    r[N - 1] = inv(f[N - 1]);
+    for (int i = N - 2; i > -1; i--)
+        r[i] = mul(r[i + 1], i + 1);
+}
+
+int C(int n, int k) {
+    if (n < 0 || k < 0 || n < k)
         return 0;
-    return ((fact[n] * invfact[r]) % mod * invfact[n - r]) % mod;
+    return mul(f[n], mul(r[k], r[n - k]));
 }
