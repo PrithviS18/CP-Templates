93cf27c5e685887e1ab4eb53c3374260cd5092e8
Prithvi Sen <113365172+PrithviS18@users.noreply.github.com>
2025-12-07 18:54:45 +0530

    Update Trie.cpp

--- Tries/Trie.cpp
+++ Tries/Trie.cpp
@@ -1,90 +1,54 @@
 class TrieNode {
 public:
-    TrieNode* children[2];
+    TrieNode* children[26];
     int maxx;
+    bool flag;
 
     TrieNode (){
-        children[0]=children[1]=nullptr;
+        for (int i=0;i<26;i++)children[i]=nullptr;
         maxx=-1;
+        flag=false;
     }
 };
 
 class Trie {
 private:
     TrieNode* root;
-
 public:
-    Trie(){
+    Trie() {
         root = new TrieNode();
     }
-
-    void insert(ll val, int i){
+    
+    void insert(string word) {
         TrieNode* node = root;
-        ll power = 1073741824;
-        while (power!=0){
-            int index;
-            ((val&power)==0?index=0:index=1);
+        for (int i=0;i<word.size();i++){
+            int index=word[i]-'a';
             if (!node->children[index]){
-                node->children[index] = new TrieNode();
+                node->children[index]=new TrieNode();
             }
             node = node->children[index];
-            node->maxx = max(node->maxx,i);
-            power/=2;
         }
+        node->flag=true;
     }
-
-    int nearestIndex (ll val, ll k){
-        TrieNode * node = root;
-        ll power = 1073741824;
-        int ans = -1;
-        while(power!=0 && node!=nullptr){
-            ll ind,ind1;
-            ((val&power)==0?ind=0:ind=1);
-            ((k&power)==0?ind1=0:ind1=1);
-            // cout<<ind<<" "<<ind1<<endl;
-            if (ind==1 && ind1==1){
-                node = node->children[0];
-                if (power==1 && node!=nullptr)ans=max(ans,node->maxx);
-            }
-            else if (ind==0 && ind1==0){
-                if (node->children[1]!=nullptr)ans=max(ans,node->children[1]->maxx);
-                node = node->children[0];
-                if (power==1 && node!=nullptr)ans=max(ans,node->maxx);
-            }
-            else if (ind==0 && ind1==1){
-                node = node->children[1];
-                if (power==1 && node!=nullptr)ans=max(ans,node->maxx);
-            }
-            else {
-                if (node->children[0]!=nullptr)ans=max(ans,node->children[0]->maxx);
-                node = node->children[1];
-                if (power==1 && node!=nullptr)ans=max(ans,node->maxx);
-            }
-            power/=2;
+    
+    bool search(string word) {
+        TrieNode* node = root;
+        for (int i=0;i<word.size();i++){
+            int index = word[i]-'a';
+            if (!node->children[index])return false;
+            node = node->children[index];
         }
-        return ans;
-    }  
+        if (node->flag)return true;
+        return false;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* node = root;
+        for (int i=0;i<prefix.size();i++){
+            int index = prefix[i]-'a';
+            if (!node->children[index])return false;
+            node = node->children[index];
+        }
+        return true;
+    }
 };
-
-void solve(int t) {
-    ll n,k;
-    cin>>n>>k;
-    ll arr[n];
-    for (int i=0;i<n;i++)cin>>arr[i];
-    if (k==0){
-        cout<<1<<endl;
-        return;
-    }
-    Trie trie;
-    int ans=INT_MAX;
-    for (int i=0;i<n;i++){
-        if (i==0)trie.insert(arr[i],i);
-        else{
-            int val = trie.nearestIndex(arr[i],k);
-            // cout<<val<<" ";
-            if (val!=-1)ans=min(ans,i-val+1);
-            trie.insert(arr[i],i);
-        }
-    }
-    (ans==INT_MAX?cout<<-1<<endl:cout<<ans<<endl);
-}
